d2a2ad8b3db87bf2068698cc585bc93f1a84a461
Derek Ho <derek.ho@prutech.com>
2020-02-26 23:07:47 -0500

    Add auth guard to route

---
+++ Timekeeper/TimekeepingWeb/src/app/guards/auth.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { SecurityService } from '../services/security.service';
+import { AlertifyService } from '../services/alertify.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: SecurityService, private router: Router,
+              private alertify: AlertifyService) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      if (this.authService.loggedIn()) {
+        return true;
+      }
+      this.alertify.error('Permission Error');
+      this.router.navigate(['/home']);
+      return false;
+  }
+}
--- Timekeeper/TimekeepingWeb/src/app/routes.routing.ts
+++ Timekeeper/TimekeepingWeb/src/app/routes.routing.ts
@@ -3,12 +3,19 @@ import { HomeComponent } from './components/home/home.component';
 import { TeamComponent } from './components/team/team.component';
 import { TimesheetComponent } from './components/timesheet/timesheet.component';
 import { UserMessagesComponent } from './components/user-messages/user-messages.component';
-
+import { AuthGuard } from './guards/auth.guard';
 
 export const appRoutes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'team', component: TeamComponent },
-  { path: 'timesheet', component: TimesheetComponent},
-  { path: 'messages', component: UserMessagesComponent},
+  {path: 'home', component: HomeComponent},
+  {
+      path: '',
+      runGuardsAndResolvers: 'always',
+      canActivate: [AuthGuard],
+      children: [
+        { path: 'team', component: TeamComponent},
+        { path: 'timesheet', component: TimesheetComponent},
+        { path: 'messages', component: UserMessagesComponent},
+      ]
+  },
   { path: '**', redirectTo: 'home', pathMatch: 'full'}
 ];
